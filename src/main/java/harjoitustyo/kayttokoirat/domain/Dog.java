e1b94ffc448de7b2d778c5c96ed3e7ee142f294f
Salminsi <salminsi@hotmail.com>
2025-10-13 01:07:31 +0300

    hahmoteltu, sovellusta ei voi viela kaynnistaa

--- src/main/java/harjoitustyo/kayttokoirat/domain/Dog.java
+++ src/main/java/harjoitustyo/kayttokoirat/domain/Dog.java
@@ -1,5 +1,45 @@
 package harjoitustyo.kayttokoirat.domain;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
 public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Nimi ei voi olla tyhjä.")
+    @Size(min = 1, max = 50, message = "Kirjaimia tulee olla 1-50.")
+    private String dogname;
+
+    @ManyToOne
+    // private breed_id; //breedid tulee tietokannasta/foreignkey
+
+    @NotEmpty(message = "Sukupuoli ei voi olla tyhjä.")
+    private String gender;
+
+    @Size(min = 0, max = 4, message = "Numeroita tulee olla korkeintaan 4.")
+    private int birthyear;
+
+    @NotEmpty(message = "Omistaja ei voi olla tyhjä.")
+    @Size(min = 2, max = 50, message = "Kirjaimia tulee olla 1-50.")
+    private String owner;
+
+    private String activities;
+
+    @Size(min = 0, max = 500, message = "Kirjaimia saa olla maksimissaan 500.")
+    private String description;
+
+    // rotu voisi tulla "kategoriasta" breed/breedRepository. Miten saa uuden
+    // lisättyä tai "muu"?
+    // entä lajit? Olisiko myös pudotusvalikko? Samoin sukupuoli??
 
 }
--- src/main/java/harjoitustyo/kayttokoirat/web/DogController.java
+++ src/main/java/harjoitustyo/kayttokoirat/web/DogController.java
@@ -1,6 +1,16 @@
 package harjoitustyo.kayttokoirat.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import harjoitustyo.kayttokoirat.domain.DogRepository;
+import jakarta.validation.Valid;
+
+
 
 
 
@@ -8,4 +18,57 @@ import org.springframework.stereotype.Controller;
 
 public class DogController {
 
+@Autowired
+private DogRepository dogRepository;
+
+
+@GetMapping("/main")
+public String Main(Model model) {
+    model.addAttribute("dogs", dogRepository.findAll());
+    return "main";
 }
+
+
+@GetMapping("/add")
+public String addDogForm(Model model) {
+    model.addAttribute("dog", new Dog());
+    model.addAttribute("breeds", breedRepository.findAll());
+    return "adddog";
+}
+
+@PostMapping("/save")
+public String save(@Valid @ModelAttribute("dog") Dog dog, BindingResult bindingResult, Model model) {
+    if(bindingResult.hasErrors()) {
+        System.out.println("Errors errors " + dog);
+        model.addAttribute(dog, dog);
+        model.addAttribute(breeds, breedRepository.findAll());
+        return "adddog";  //JOS VIRHE TULEE MINNE PALATAAN?
+    }
+    System.out.println("Save " + dog);
+    dogRepository.save(dog);
+    return "redirect:/main";
+}
+
+
+}
+
+
+
+
+/* 
+<input type="text" th:field="*{dogname}"/>
+     <!-- Rotu-pudotusvalikko tähän -->
+     <!-- <select th:field="*{breed}" >    -->
+     <!--          <option th:each="cat: ${breeds}" th:value="${breed.id}" th:text="${breed.name}"></option>   -->
+     <!--   </select>  -->
+
+    <input type="text" th:field="*{gender}"/>
+    <input type="text" th:field="*{birthyear}"/>
+    <input type="text" th:field="*{owner}"/>
+    <input type="text" th:field="*{activities}"/>
+    <input type="text" th:field="*{description}"/>
+
+    <input type="submit" value="Tallenna"/>
+
+
+    */
